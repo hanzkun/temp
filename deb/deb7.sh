b12df3d32ccf28cd1412ebfae0e8f58f6dd37d7a
hanzkun <afahandler@gmail.com>
2014-03-08 23:35:59 +0800

    Update deb7.sh

--- deb/deb7.sh
+++ deb/deb7.sh
@@ -14,8 +14,7 @@ echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
 
 # install wget and curl
-apt-get update
-apt-get -y install curl
+apt-get -y install wget curl
 
 # set time GMT +7
 #ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
