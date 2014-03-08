bb214125883fefbafcb45f5fe798e5ccb492a364
hanzkun <afahandler@gmail.com>
2014-03-08 23:12:47 +0800

    Update deb7.sh

--- deb/deb7.sh
+++ deb/deb7.sh
@@ -1,32 +1,38 @@
-#!binbash
+#!/bin/sh
+
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0'`;
+MYIP2="s/xxxxxxxxx/$MYIP/g";
 
 # go to root
 cd
 
 # disable ipv6
-echo 1  procsysnetipv6confalldisable_ipv6
-sed -i '$ iecho 1  procsysnetipv6confalldisable_ipv6' etcrc.local
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
 
 # install wget and curl
 apt-get update;apt-get -y install wget curl;
 
 # set time GMT +7
-#ln -fs usrsharezoneinfoAsiaJakarta etclocaltime
+#ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
 # set locale
-sed -i 'sAcceptEnv#AcceptEnvg' etcsshsshd_config
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 service ssh restart
 
 # set repo
-wget -O etcaptsources.list httpsraw.github.comhanzkuntempmasterdebconfsources.list.deb7
-wget httpwww.dotdeb.orgdotdeb.gpg
-cat dotdeb.gpg  apt-key add -;rm dotdeb.gpg
+wget -O /etc/apt/sources.list "https://raw.github.com/hanzkun/temp/master/deb/conf/sources.list.deb7"
+wget "http://www.dotdeb.org/dotdeb.gpg"
+cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
 
 # remove unused
-apt-get -y --purge remove samba;
-apt-get -y --purge remove apache2;
-apt-get -y --purge remove sendmail;
-apt-get -y --purge remove bind9;
+apt-get -y --purge remove samba*;
+apt-get -y --purge remove apache2*;
+apt-get -y --purge remove sendmail*;
+apt-get -y --purge remove bind9*;
 
 # update
 apt-get update; apt-get -y upgrade;
@@ -35,6 +41,7 @@ apt-get update; apt-get -y upgrade;
 #apt-get -y install nginx php5-fpm php5-cli
 
 # install essential package
+#echo "mrtg mrtg/conf_mods boolean true" | debconf-set-selections
 apt-get -y install bmon iftop htop nmap axel nano iptables traceroute sysv-rc-conf dnsutils bc nethogs vnstat less screen psmisc apt-file whois ptunnel ngrep mtr git zsh snmp snmpd snmp-mibs-downloader unzip unrar rsyslog debsums rkhunter
 apt-get -y install build-essential
 
@@ -51,54 +58,85 @@ service vnstat restart
 
 # install screenfetch
 cd
-wget httpsgithub.comKittyKattscreenFetchrawmasterscreenfetch-dev
-mv screenfetch-dev usrbinscreenfetch
-chmod +x usrbinscreenfetch
-echo clear  .profile
-echo screenfetch  .profile
+wget --no-check-certificate "https://github.com/KittyKatt/screenFetch/raw/master/screenfetch-dev"
+mv screenfetch-dev /usr/bin/screenfetch
+chmod +x /usr/bin/screenfetch
+echo "clear" >> .profile
+echo "screenfetch" >> .profile
+
 
 # install badvpn
-cd
-wget -O usrbinbadvpn-udpgw httpsraw.github.comhanzkuntempmasterdebconfbadvpn-udpgw
-sed -i '$ iscreen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.17300' etcrc.local
-chmod +x usrbinbadvpn-udpgw
-screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.17300
-
+wget -O /usr/bin/badvpn-udpgw "http://172.245.223.98/volcanos/badvpn-udpgw"
+fi
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+chmod +x /usr/bin/badvpn-udpgw
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
 
 # setting port ssh
-sed -i 'Port 22a Port 443' etcsshsshd_config
-sed -i 'Port 22a Port 80' etcsshsshd_config
-sed -i 'sPort 22Port  22g' etcsshsshd_config
+cd
+sed -i '/Port 22/a Port 443' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port  80' /etc/ssh/sshd_config
+sed -i 's/Port 22/Port  22/g' /etc/ssh/sshd_config
 service ssh restart
 
 # install dropbear
 apt-get -y install dropbear
-sed -i 'sNO_START=1NO_START=0g' etcdefaultdropbear
-sed -i 'sDROPBEAR_PORT=22#DROPBEAR_PORT=22' etcdefaultdropbear
-sed -i 'sDROPBEAR_EXTRA_ARGS=DROPBEAR_EXTRA_ARGS=-p 109 -p 143g' etcdefaultdropbear
-echo binfalse  etcshells
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/#DROPBEAR_PORT=22/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109 -p 143"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
 service ssh restart
 service dropbear restart
+
+# install vnstat gui
+cd /home/vps/public_html/
+wget http://www.sqweek.com/sqweek/files/vnstat_php_frontend-1.5.1.tar.gz
+tar xf vnstat_php_frontend-1.5.1.tar.gz
+rm vnstat_php_frontend-1.5.1.tar.gz
+mv vnstat_php_frontend-1.5.1 vnstat
+cd vnstat
+sed -i 's/eth0/venet0/g' config.php
+sed -i "s/\$iface_list = array('venet0', 'sixxs');/\$iface_list = array('venet0');/g" config.php
+sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+sed -i 's/Internal/Internet/g' config.php
+sed -i '/SixXS IPv6/d' config.php
 cd
 
 # install fail2ban
 apt-get -y install fail2ban;service fail2ban restart
 
-# install squid3
-apt-get -y install squid3
-wget -O etcsquid3squid.conf http172.245.223.98volcanossquid.conf
-sed -i $MYIP2 etcsquid3squid.conf;
-service squid3 restart
+# install squid
+apt-get -y install squid
+wget -O /etc/squid/squid.conf "http://172.245.223.98/volcanos/squid.conf"
+sed -i $MYIP2 /etc/squid/squid.conf;
+service squid restart
 
 # install webmin
 cd
-wget httpprdownloads.sourceforge.netwebadminwebmin_1.670_all.deb
+wget "http://script.jualssh.com/autoscript/conf/webmin_1.670_all.deb"
 dpkg --install webmin_1.670_all.deb;
 apt-get -y -f install;
-rm rootwebmin_1.670_all.deb
+rm /root/webmin_1.670_all.deb
 service webmin restart
 service vnstat restart
 
+# downlaod script
+cd
+wget --no-check-certificate -O speedtest_cli.py "https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py"
+wget -O bench-network.sh "http://script.jualssh.com/autoscript/conf/bench-network.sh"
+wget --no-check-certificate -O ps_mem.py "https://raw.github.com/pixelb/ps_mem/master/ps_mem.py"
+wget -O limit.sh "http://172.245.223.98/volcanos/limit.sh"
+curl http://172.245.223.98/volcanos/volcanos.sh > user-login.sh
+curl http://172.245.223.98/volcanos/limit.sh > user-limit.sh
+echo "0 0 * * * root /root/user-expire.sh" > /etc/cron.d/user-expire
+sed -i '$ i\screen -AmdS limit /root/limit.sh' /etc/rc.local
+chmod +x bench-network.sh
+chmod +x speedtest_cli.py
+chmod +x ps_mem.py
+chmod +x user-login.sh
+chmod +x user-expire.sh
+chmod +x user-limit.sh
+chmod +x limit.sh
 
 # finalisasi
 service vnstat restart
@@ -106,24 +144,35 @@ service snmpd restart
 service ssh restart
 service dropbear restart
 service fail2ban restart
-service squid3 restart
+service squid restart
 service webmin restart
 
 # info
 clear
-echo brumbrumrbumbrum.......  tee log-install.txt
-echo ===============================================  tee -a log-install.txt
-echo    tee -a log-install.txt
-echo Service   tee -a log-install.txt
-echo -------   tee -a log-install.txt
-echo OpenSSH   22, 80, 443   tee -a log-install.txt
-echo Dropbear  109, 143   tee -a log-install.txt
-echo Squid3    8080 (limit to IP SSH)   tee -a log-install.txt
-echo badvpn    badvpn-udpgw port 7300   tee -a log-install.txt
-echo    tee -a log-install.txt
-echo Script   tee -a log-install.txt
-echo ------   tee -a log-install.txt
-echo screenfetch   tee -a log-install.txt
-echo SILAHKAN REBOOT VPS ANDA !   tee -a log-install.txt
-echo    tee -a log-install.txt
-echo ===============================================   tee -a log-install.txt
+echo "blablablbala" | tee log-install.txt
+echo "===============================================" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "Service"  | tee -a log-install.txt
+echo "-------"  | tee -a log-install.txt
+echo "OpenSSH  : 22, 80, 443"  | tee -a log-install.txt
+echo "Dropbear : 109, 143"  | tee -a log-install.txt
+echo "Squid    : 8080 (limit to IP SSH)"  | tee -a log-install.txt
+echo "badvpn   : badvpn-udpgw port 7300"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "Tools"  | tee -a log-install.txt
+echo "-----"  | tee -a log-install.txt
+echo "axel"  | tee -a log-install.txt
+echo "bmon"  | tee -a log-install.txt
+echo "htop"  | tee -a log-install.txt
+echo "iftop"  | tee -a log-install.txt
+echo "mtr"  | tee -a log-install.txt
+echo "nethogs"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "Fail2Ban : [on]"  | tee -a log-install.txt
+echo "IPv6     : [off]"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "Log Installasi --> /root/log-install.txt"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "SILAHKAN REBOOT VPS ANDA !"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "==============================================="  | tee -a log-install.txt
