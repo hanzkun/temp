4e4d2c9f008b174dde7c6086925738bbbe6314f7
hanzkun <afahandler@gmail.com>
2014-03-09 03:16:02 +0800

    Update deb7.sh

--- deb/deb7.sh
+++ deb/deb7.sh
@@ -74,7 +74,7 @@ screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
 # setting port ssh
 cd
 sed -i '/Port 22/a Port 443' /etc/ssh/sshd_config
-sed -i '/Port 22/a Port  80' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 80' /etc/ssh/sshd_config
 sed -i 's/Port 22/Port  22/g' /etc/ssh/sshd_config
 service ssh restart
 
