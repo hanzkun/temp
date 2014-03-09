9a0894da63c67cec2783eeb2f042bd07c01d991a
hanzkun <afahandler@gmail.com>
2014-03-10 05:51:27 +0800

    Update and rename test.sh to test2.sh

--- test2.sh
+++ test2.sh
@@ -5,8 +5,8 @@ Port="8080 80"
 #apt-get -y install squid3
 #squid without password
 #lock only for ip server
-mv /etc/squid/squid.conf /etc/squid/squid.conf.backup
+mv /etc/squid3/squid.conf /etc/squid/squid.conf.backup
 wget repo.vpscepat.net/squid.conf
-mv squid.conf /etc/squid/squid.conf
-sed -i s/123.123.123.123/$IP/g /etc/squid/squid.conf
+mv squid.conf /etc/squid3/squid.conf
+sed -i s/123.123.123.123/$IP/g /etc/squid3/squid.conf
 service squid restart
